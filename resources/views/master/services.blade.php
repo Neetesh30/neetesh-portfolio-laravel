994bfb9f96c33bd57067fd61baa8ee23fb3b77fb
Neetesh30 <neetesh652@gmail.com>
2021-06-27 13:17:07 +0530

    services section completed

--- resources/views/master/services.blade.php
+++ resources/views/master/services.blade.php
@@ -9,49 +9,58 @@
       <div class="row">
         <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
           <div class="icon-box">
-            <div class="icon"><i class="bx bxl-dribbble"></i></div>
-            <h4><a href="">Lorem Ipsum</a></h4>
-            <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+            <div class="icon"><i class='bx bx-layout' ></i></div>
+            <h4><a href="">WEBSITE DEVELOPMENT</a></h4>
+            <p>We understand that a website stands as the face of your business and 
+              hence we create bespoke website designs that help you make an impactful first impression.</p>
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
           <div class="icon-box">
-            <div class="icon"><i class="bx bx-file"></i></div>
-            <h4><a href="">Sed ut perspiciatis</a></h4>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+            <div class="icon"><i class='bx bxs-shopping-bags'></i></div>
+            <h4><a href="">E-COMMERCE WEBSITE DEVELOPMENT</a></h4>
+            <p>We help you create a 24x7 fully optimized online store for your brand so that at any point in 
+              time you do not miss out on a sale.
+            </p>
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
           <div class="icon-box">
-            <div class="icon"><i class="bx bx-tachometer"></i></div>
-            <h4><a href="">Magni Dolores</a></h4>
-            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+            <div class="icon"><i class='bx bxs-layout'></i></div>
+            <h4><a href="">LANDING PAGE DEVELOPMENT</a></h4>
+            <p>Provide the best landing page services that are not only conversion-oriented but are also fully responsive and possess a fast load time.
+            </p>
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
           <div class="icon-box">
-            <div class="icon"><i class="bx bx-world"></i></div>
-            <h4><a href="">Nemo Enim</a></h4>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+            <div class="icon"><i class='bx bx-search-alt'></i></div>
+            <h4><a href="">SEARCH ENGINE OPTIMIZATION</a></h4>
+            <p>Implement the foundational search engine elements needed to drive visibility and 
+              boost your websites’ visibility on search engine result pages.
+            </p>
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
           <div class="icon-box">
-            <div class="icon"><i class="bx bx-slideshow"></i></div>
-            <h4><a href="">Dele cardo</a></h4>
-            <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
+            <div class="icon"><i class='bx bxs-network-chart'></i></div>
+            <h4><a href="">SOCIAl MEDIA MARKETING</a></h4>
+            <p>Make use of a mix of creative marketing techniques that help brands create 
+              a strong social media presence across different platforms.
+            </p>
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
           <div class="icon-box">
-            <div class="icon"><i class="bx bx-arch"></i></div>
-            <h4><a href="">Divera don</a></h4>
-            <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
+            <div class="icon"><i class='bx bx-refresh'></i></div>
+            <h4><a href="">WEBSITE REDESIGN & OPTIMIZATION</a></h4>
+            <p>A poorly built website can push your customers away. we give your existing website a 360° makeover.
+            </p>
           </div>
         </div>
 
